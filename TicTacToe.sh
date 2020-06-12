b1537b54679bcd87fbdadfbb24d88ad0b6cc5a59
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-12 23:20:38 +0530

    Added: Display function

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -40,6 +40,20 @@ function assignLetter() {
 		computer=o
 	fi
 }
+function display() {
+	echo "		***TicTacToe gameBoard***"
+	echo
+	echo
+	echo "		     ${gameBoard[0,0]}  |  ${gameBoard[0,1]}  |  ${gameBoard[0,2]}  "
+	echo "		   -----------------"
+	echo "		     ${gameBoard[1,0]}  |  ${gameBoard[1,1]}  |  ${gameBoard[1,2]}  "
+	echo "		   -----------------"
+	echo "	 	     ${gameBoard[2,0]}  |  ${gameBoard[2,1]}  |  ${gameBoard[2,2]}  "
+	echo
+	echo
+	echo "		*************************"
+}
 reset
 whoPlayFirst
 assignLetter
+display
