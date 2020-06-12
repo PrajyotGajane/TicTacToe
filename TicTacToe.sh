7e2327c7f45a236743f2304bc8700ab1cf062557
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-12 23:09:29 +0530

    Added: UC1 Reset Board function

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,2 +1,27 @@
 #!/bin/bash -x
 echo "Weclome to Tic Tac Toe Game"
+#variables
+block=0
+count=0
+player=0
+computer=x
+changeTurn=$player
+winner=0
+playerSwitch=1
+position=1
+#array declaration
+rowSize=3
+columnSize=3
+declare -A gameBoard
+function reset() {
+	for (( row=0; row<$rowSize; row++ ))
+	do
+		for(( column=0; column<$columnSize; column++ ))
+		do
+			#echo $row
+			#echo $column
+			gameBoard[$row,$column]="."
+		done
+	done
+}
+reset
